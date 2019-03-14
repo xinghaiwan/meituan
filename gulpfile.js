e70fbd18a0ef7adfc3eaec7ebdcab57bb1f2e750
liyueyang <l13717920694@163.com>
2019-03-14 14:20:53 +0800

    done

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,16 @@ const sass = require('gulp-sass');
 const server = require('gulp-webserver');
 const minCss = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const data = require('./src/data/data.json');
+const list = require('./src/data/list.json');
 gulp.task('sass', function() {
     return gulp.src('./src/scss/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./src/'))
+        .pipe(gulp.dest('./src/css/'))
 });
 
 gulp.task('watch', function() {
@@ -18,7 +24,13 @@ gulp.task('minCss', function() {
         .pipe(minCss())
         .pipe(gulp.dest('./src/css/'))
 });
-
+gulp.task('babel', function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(babel({
+            presets: 'es2015'
+        }))
+        .pipe(gulp.dest('./src/js/'));
+});
 gulp.task('uglify', function() {
     return gulp.src('./src/js/*.js')
         .pipe(uglify())
@@ -30,7 +42,19 @@ gulp.task('server', function() {
         .pipe(server({
             port: 8787,
             open: true,
-            livereload: true
-        }))
+            livereload: true,
+            middleware: function(req, res, next) {
+                let { pathname, query } = url.parse(req.url, true);
+                if (pathname == '/favicon.ico') {
+                    res.end('');
+                    return;
+                } else if (pathname == '/api/list') {
+                    res.end(JSON.stringify({ code: 1, data: data, list: list }));
+                } else {
+                    pathname = pathname === '/' ? 'index.html' : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname, '/src', pathname)));
+                }
+            }
+        }));
 });
-gulp.task('dev', gulp.series('sass', 'minCss', 'uglify', 'server', 'watch'));
+gulp.task('dev', gulp.series('sass', 'minCss', 'babel', 'uglify', 'server', 'watch'));
